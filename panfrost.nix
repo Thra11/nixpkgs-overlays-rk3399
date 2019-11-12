5451deb4be3cfe15e3015e888d850a1ab5395112
Tom Hall <tahall256@protonmail.ch>
2019-11-12 11:43:44 +0000

    panfrost: Switch to 19.2.0 mesa release
    Only override mesa if the super's mesa doesn't meet the minimum version
    required for panfrost to work.
    
    Drop libdrm, glu, kmscube: Any fairly recent version should be sufficient

--- panfrost.nix
+++ panfrost.nix
@@ -1,42 +1,29 @@
 self: super:
 {
-  # Building the following 4 packages from git sources was required for panfrost support.
-  # There have probably been releases since, so need to confirm what the minimum versions required are.
-  libdrm = super.libdrm.overrideAttrs (o: {
-    src = super.fetchgit {
-      url = "https://gitlab.freedesktop.org/mesa/drm.git";
-      rev = "14922551aa33e7592d2421cc89cf20a860a65310";
-      sha256 = "1aly2bbmp64f9yaj5pnj1nw25vgb0rzpw1n3kif6ngxxsq1x5r19";
-    };
-  });
-  mesa_glu = super.mesa_glu.overrideAttrs (o: {
-    src = super.fetchgit {
-      url = "https://gitlab.freedesktop.org/mesa/${o.pname}.git";
-      rev = "dd4e18eb7557a31a3c8318d6612801329877c745";
-      sha256 = "12ds0l9lasabzkqfm65ihhjc0zq9w2rgz2ijkcfr3njgpzjs367n";
-    };
-    nativeBuildInputs = o.nativeBuildInputs ++ [ self.autoconf self.automake self.libtool ];
-    preConfigure = ''
-      ./autogen.sh
-    '';
-  });
-  mesa = super.mesa.overrideAttrs (o: {
-    src = super.fetchgit {
-      url = "https://gitlab.freedesktop.org/mesa/mesa.git";
-      rev = "74a7e3ed3b297f441b406ff62ef9ba504ba3b06c";
-      sha256 = "1lrnfgrs3qpg1awqsw15vaj70fq5z8yksfzr0cfqnvif8ia8j78m";
-    };
-    postFixup = builtins.replaceStrings [
-      "rm $dev/lib/pkgconfig/{gl,egl}.pc"
-    ] [
-      "rm $dev/lib/pkgconfig/gl.pc"
-    ] o.postFixup;
-  });
-  kmscube = super.kmscube.overrideAttrs (o: {
-    src = super.fetchgit {
-      url = git://anongit.freedesktop.org/mesa/kmscube;
-      rev = "f632b23a528ed6b4e1fddd774db005c30ab65568";
-      sha256 = "0rr58h8g1nj94ng13hdd6nn44155xg48xafyi0v843lvh09k88vh";
-    };
-  });
+  mesa =
+    let
+      minVersion = "19.2.0";
+    in
+    if (self.lib.versionOlder super.mesa.version minVersion) then (super.mesa.overrideAttrs (o:
+    let
+      version = minVersion;
+      branch  = builtins.head (self.lib.splitString "." version);
+    in
+    {
+      inherit version;
+      src =  super.fetchurl {
+        urls = [
+          "ftp://ftp.freedesktop.org/pub/mesa/mesa-${version}.tar.xz"
+          "ftp://ftp.freedesktop.org/pub/mesa/${version}/mesa-${version}.tar.xz"
+          "ftp://ftp.freedesktop.org/pub/mesa/older-versions/${branch}.x/${version}/mesa-${version}.tar.xz"
+          "https://mesa.freedesktop.org/archive/mesa-${version}.tar.xz"
+        ];
+        sha256 = "0al5235xrsv5walyavn192mg4n4g1rykgxqg2qqn918gl2iclq5h";
+      };
+      postFixup = builtins.replaceStrings [
+        "rm $dev/lib/pkgconfig/{gl,egl}.pc"
+      ] [
+        "rm $dev/lib/pkgconfig/gl.pc"
+      ] o.postFixup;
+    })) else super.mesa;
 }
