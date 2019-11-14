c7ae3221547b56919e2e93412d1820b88e9b8005
Tom Hall <tahall256@protonmail.ch>
2019-11-14 17:16:59 +0000

    Add sd-image

---
+++ sd-image/sd-image-rockpro64.nix
@@ -0,0 +1,62 @@
+# To build, use:
+# nix-build <nixpkgs/nixos> -I nixos-config=./sd-image-rockpro64.nix -A config.system.build.sdImage
+{ config, lib, pkgs, ... }:
+
+let
+  extlinux-conf-builder =
+    import <nixpkgs/nixos/modules/system/boot/loader/generic-extlinux-compatible/extlinux-conf-builder.nix> {
+      pkgs = pkgs.buildPackages;
+    };
+in
+{
+  imports = [
+    <nixpkgs/nixos/modules/profiles/base.nix>
+    <nixpkgs/nixos/modules/profiles/installation-device.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/sd-image.nix>
+  ];
+
+  nixpkgs.overlays = let
+    uboot = import ../uboot.nix;
+    kernel = import ../kernel.nix;
+    rockchip = import ../rockchip.nix;
+    panfrost = import ../panfrost.nix;
+  in [
+    uboot
+    kernel
+    rockchip
+    panfrost
+  ];
+
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+
+  boot.consoleLogLevel = lib.mkDefault 7;
+  boot.kernelPackages = pkgs.linuxPackages_rk3399_5_4;
+  boot.kernelParams = [ "console=uart8250,mmio32,0xff1a0000" "console=ttyS2,1500000n8" "video=eDP-1:1920x1080@60" ];
+
+  sdImage = {
+    populateFirmwareCommands = let
+      configTxt = pkgs.writeText "config.txt" ''
+        debug=on
+      '';
+      in ''
+        dd if=${pkgs.ubootRockPro64}/idbloader.img of=$img bs=512 seek=64 oflag=sync conv=notrunc
+        cp ${configTxt} firmware/config.txt
+      '';
+    populateRootCommands = ''
+      mkdir -p ./files/boot
+      ${extlinux-conf-builder} -t 3 -c ${config.system.build.toplevel} -d ./files/boot
+    '';
+  };
+
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "panfrost" ];
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
+    desktopManager.plasma5.enableQt4Support = false;
+    #xserver.layout = "us";
+    #xserver.xkbOptions = "eurosign:e";
+    libinput.enable = true;
+  };
+}
