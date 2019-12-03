247aeadcb59e1a0ccbe89cd6e0d1c8aa2021978c
Tom Hall <tahall256@protonmail.ch>
2019-12-03 22:26:11 +0000

    ap6256-firmware: Fix install paths and update

--- pkgs/ap6256-firmware/default.nix
+++ pkgs/ap6256-firmware/default.nix
@@ -2,20 +2,21 @@
 
 stdenv.mkDerivation rec {
   pname = "ap6256-firmware";
-  version = "2019-08";
+  version = "2019-11";
 
   dontUnpack = true;
   dontFixup = true;
 
   installPhase = ''
     mkdir -p $out/lib/firmware
-    install -Dm644 "${./BCM4345C5.hcd}" -t "$out/lib/firmware/"
+    install -Dm644 "${./BCM4345C5.hcd}" "$out/lib/firmware/BCM4345C5.hcd"
+    install -Dm644 "${./BCM4345C5.hcd}" "$out/lib/firmware/brcm/BCM4345C5.hcd"
     install -Dm644 "${./BCM4345C5.hcd}" "$out/lib/firmware/brcm/BCM.hcd"
-    install -Dm644 "${./nvram_ap6256.txt}" -t "$out/lib/firmware/"
-    # Wfi fix because of weird setup
+    install -Dm644 "${./nvram_ap6256.txt}" "$out/lib/firmware/nvram_ap6256.txt"
     install -Dm644 "${./fw_bcm43456c5_ag.bin}" "$out/lib/firmware/brcm/brcmfmac43456-sdio.bin"
     install -Dm644 "${./nvram_ap6256.txt}" "$out/lib/firmware/brcm/brcmfmac43456-sdio.radxa,rockpi4.txt"
     install -Dm644 "${./nvram_ap6256.txt}" "$out/lib/firmware/brcm/brcmfmac43456-sdio.pine64,pinebook-pro.txt"
+    install -Dm644 "${./brcmfmac43456-sdio.clm_blob}" "$out/lib/firmware/brcm/brcmfmac43456-sdio.clm_blob"
   '';
   meta = with stdenv.lib; {
     description = "Firmware files for the ap6256 wifi/bt module";
